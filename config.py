2f7162e76e149dfa696ea71a0e559ca2a9ac6f6f
tigerhhlive <hossein.had@outlook.com>
2025-04-15 19:55:57 +0330

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# توکن تلگرام
+TELEGRAM_TOKEN = '7799983500:AAGKaoEv5SIE1Ou_dXwib7daEZd9gJWHSfA'
+
+# شناسه چت تلگرام
+CHAT_ID = '@CryptoLivecheck'
+
+# کلید API NewsAPI
+NEWS_API_KEY = '3788a1f05c7d472a94700d5c35cd465f'
