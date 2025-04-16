e4bb68763984f506c97acffc6a4b86edff21c8bd
tigerhhlive <hossein.had@outlook.com>
2025-04-16 14:28:55 +0330

    config.py

--- config.py
+++ config.py
@@ -11,5 +11,8 @@ CHAT_ID = os.environ.get('CHAT_ID')
 # کلید API NewsAPI
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
-# کلمات کلیدی حساس برای شناسایی اخبار مهم
+# کلمات حساس مرتبط با پامپ/دامپ
 SENSITIVE_KEYWORDS = ['pump', 'surge', 'spike', 'skyrocketing', 'dump', 'fall', 'crash', 'drop']
+
+# منابع معتبر خبری
+TRUSTED_SOURCES = ['CoinDesk', 'Cointelegraph', 'Bloomberg', 'Forbes', 'Yahoo', 'Reuters']
